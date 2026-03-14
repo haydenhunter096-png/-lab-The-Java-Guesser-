ba87826ee59785c0d9cf16b4c66c469d0e285ad0
haydenhunter096-png <haydenhunter096@gmail.com>
2026-03-14 04:41:04 +0000

    update guesser program

--- Guesser.java
+++ Guesser.java
@@ -2,103 +2,87 @@ import java.util.Scanner;
 
 public class Guesser {
 
-    private Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
-        Guesser g = new Guesser();
-        g.run();
-    }
 
-    public void run() {
-        int choice;
+        Scanner input = new Scanner(System.in);
+        int choice = -1;
 
-        do {
+        while (choice != 0) {
+
             System.out.println("0) Exit");
             System.out.println("1) Human Guesser");
             System.out.println("2) Computer guesser");
             System.out.print("\nPlease enter 0-2: ");
 
-            choice = scanner.nextInt();
+            choice = input.nextInt();
 
             if (choice == 1) {
-                humanGuesser();
-            } else if (choice == 2) {
-                computerGuesser();
+
+                System.out.println("human guesser");
+
+                int number = (int)(Math.random() * 100) + 1;
+                int guess = 0;
+                int count = 1;
+
+                while (guess != number) {
+
+                    System.out.print(count + ") Please enter a number: ");
+                    guess = input.nextInt();
+
+                    if (guess < number) {
+                        System.out.println("too low...");
+                    }
+                    else if (guess > number) {
+                        System.out.println("too high...");
+                    }
+                    else {
+                        System.out.println("got it!");
+                        System.out.println("You guessed the number in " + count + " guesses!");
+                        System.out.println("\nVery good!");
+                    }
+
+                    count++;
+                }
+            }
+
+            else if (choice == 2) {
+
+                System.out.println("computer guesser");
+
+                int low = 1;
+                int high = 100;
+                int guess;
+                int count = 1;
+
+                input.nextLine();
+
+                while (true) {
+
+                    guess = (low + high) / 2;
+
+                    System.out.println(count + ") I guess " + guess);
+                    System.out.print("Too (H)igh, too (L)ow, or (C)orrect? ");
+
+                    String response = input.nextLine();
+
+                    if (response.equalsIgnoreCase("c")) {
+                        System.out.println("I found your number in " + count + " guesses!");
+                        break;
+                    }
+                    else if (response.equalsIgnoreCase("h")) {
+                        high = guess - 1;
+                    }
+                    else if (response.equalsIgnoreCase("l")) {
+                        low = guess + 1;
+                    }
+
+                    count++;
+                }
             }
 
             System.out.println();
+        }
 
-        } while (choice != 0);
-    }
-
-    public void humanGuesser() {
-        char playAgain;
-
-        do {
-            System.out.println("human guesser");
-
-            int target = (int) (Math.random() * 100) + 1;
-            int guess = 0;
-            int attempt = 1;
-
-            while (guess != target) {
-                System.out.print(attempt + ") Please enter a number: ");
-                guess = scanner.nextInt();
-
-                if (guess < target) {
-                    System.out.println("too low...");
-                } else if (guess > target) {
-                    System.out.println("too high...");
-                } else {
-                    System.out.println("got it!");
-                    System.out.println("\nVery good!");
-                }
-
-                attempt++;
-            }
-
-            System.out.print("Play again? (y/n): ");
-            scanner.nextLine();
-            playAgain = scanner.nextLine().toLowerCase().charAt(0);
-
-        } while (playAgain == 'y');
-    }
-
-    public void computerGuesser() {
-        char playAgain;
-
-        do {
-            System.out.println("computer guesser");
-
-            int low = 1;
-            int high = 100;
-            int attempt = 1;
-            char response;
-
-            scanner.nextLine();
-
-            while (true) {
-                int guess = (low + high) / 2;
-
-                System.out.println(attempt + ") I guess " + guess);
-                System.out.print("Too (H)igh, too (L)ow, or (C)orrect? ");
-
-                response = scanner.nextLine().toLowerCase().charAt(0);
-
-                if (response == 'c') {
-                    break;
-                } else if (response == 'h') {
-                    high = guess - 1;
-                } else if (response == 'l') {
-                    low = guess + 1;
-                }
-
-                attempt++;
-            }
-
-            System.out.print("Play again? (y/n): ");
-            playAgain = scanner.nextLine().toLowerCase().charAt(0);
-
-        } while (playAgain == 'y');
+        input.close();
     }
 }
